9e59d6a587bb57b9d974898968b136fff0f1cac4
wkdcode-aine <aine.poole@wkdcode.co.uk>
2017-11-15 17:15:13 +0000

    add the options for dropdown

---
+++ app/code/MiltonBayer/General/Model/Config/Source/Product/Options/Options.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MiltonBAyer\General\Model\Config\Source\Product\Options;
+
+/**
+ * Product option types mode source
+ */
+class Options implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * [private description]
+     * @var [type]
+     */
+    private $optionCollection;
+
+    /**
+     *
+     */
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Product\Option\Collection $optionCollection
+    ) {
+        $this->optionCollection = $optionCollection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        $groups = [['value' => '', 'label' => __('-- Please select --')]];
+
+        foreach($this->optionCollection->addTitleToResult(0)->addValuesToResult(0)->getItems() as $item) {
+            $types = [];
+
+            foreach($item->getValues() as $value) $types[] = ['label' => $value->getData('title'), 'value' => $value->getData('option_type_id')];
+
+            if( count($types) > 0 )  $groups[] = ['label' => __($item->getData('title')), 'value' => $types, 'optgroup-name' => $item->getData('title')];
+        }
+
+        return $groups;
+    }
+}
